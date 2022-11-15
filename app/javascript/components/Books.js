7951a26d5223820815d2365d49369920d1b7bc02
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-15 07:57:42 +0300

    Added add button

--- app/javascript/components/Books.js
+++ app/javascript/components/Books.js
@@ -2,57 +2,28 @@ import React, { useEffect, useState } from "react";
 import { fetchBooks } from "../redux/bookSlice";
 import { fetchUser } from "../redux/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Books() {
   const dispatch = useDispatch();
-  const [name, setName] = useState("");
   const books = useSelector((state) => state.books.books);
   const user = useSelector((state) => state.user.user);
-  const [allBooks, setAllBooks] = useState([]);
   useEffect(() => {
     dispatch(fetchBooks());
     dispatch(fetchUser());
-    setAllBooks(books);
   }, []);
-  const displayBooks = allBooks.map((book) => (
+
+  const displayBooks = books.map((book) => (
     <div key={book.id}>
       <h1>{book.name}</h1>
+      <Link to={`/books/${book.id}`}>View Book</Link>
     </div>
   ));
-
-  const addBookEvent = (e) => {
-    e.preventDefault();
-    fetch("/api/v1/books", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: name }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setAllBooks(data);
-      });
-  };
-
-  const addBook = (
-    <div>
-      <h1>Add Book</h1>
-      <input
-        type="text"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
-      <button onClick={addBookEvent}>Add</button>
-    </div>
-  );
 
   return (
     <div>
       books
       {displayBooks}
-      {/* {user.name === "admin" ? <h1>admin</h1> : <h1>not admin</h1>} */}
-      {addBook}
     </div>
   );
 }
--- app/javascript/components/EachBook.js
+++ app/javascript/components/EachBook.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchBooks } from "../redux/bookSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 function EachBook() {
-  return <div>EachBook</div>;
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.books);
+  const params = useParams();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
+
+  const displayEachBook = books.map((book) =>
+    book.id == Number(params.id) ? (
+      <div key={book.id}>
+        <h1>{book.name}</h1>
+      </div>
+    ) : null
+  );
+
+  return <div>{displayEachBook}</div>;
 }
 
 export default EachBook;
